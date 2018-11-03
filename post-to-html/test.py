accb2d73a2231571e31a219d134ca1a69d270139
Mark Youngman <mmyoungman@gmail.com>
2018-11-03 23:16:56 +0000

    Removing unnecessary stuff

--- post-to-html/test.py
+++
@@ -1,9 +0,0 @@
-from ctypes import cdll
-from ctypes import c_char_p
-
-post_to_html_lib = cdll.LoadLibrary("./post-to-html.o")
-post_to_html_lib.helloworld.restype=c_char_p
-
-name = b"Mark"
-
-print(post_to_html_lib.helloworld(name))
