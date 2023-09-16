6711f270b5bcde42e1eb78248fd1e939a95a51a0
Mark Youngman <mark@youngman.info>
2023-09-16 21:39:57 +0100

    Update live_reload.py

--- live_reload.py
+++ live_reload.py
@@ -3,8 +3,8 @@
 from livereload import Server, shell
 
 server = Server()
-server.watch('templates/*', shell('python generate_website.py'))
-server.watch('content/*', shell('python generate_website.py'))
-server.watch('public/style.css', shell('python generate_website.py'))
-server.watch('generate_website.py', shell('python generate_website.py'))
+server.watch('templates/*', shell('go run .'))
+server.watch('content/*', shell('go run .'))
+server.watch('public/style.css', shell('go run .'))
+server.watch('generate_website.go', shell('go run .'))
 server.serve(root='public/')
